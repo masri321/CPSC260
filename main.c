c752fe072e034b321e5bebd939078b8ba3f57da5
mohamed ali <mohammedali.saisdubai@gmail.com>
2013-03-02 18:15:30 -0800

    add extra comments

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//By:Mohamed Ali
+//s.54809108
+//GIT TRIAL
+int main(int argc, char * argv[])
+{
+    if(argc!= 2) //check input arguments
+    {
+        printf("ERROR: invalid input\n");
+        return -1;
+    }
+   
+   
+    unsigned long long  int input;
+    unsigned long long  int page;
+    unsigned long long  int offset;
+    
+   
+    
+    input = atol ( argv[1] ); //convert the input from char to int
+    page = input / 4096;
+    offset = input % 4096;
+
+    printf("The address %lld contains:\npage number = %lld\noffset = %lld\n",input,page, offset);
+   
+    return 0;
+}
