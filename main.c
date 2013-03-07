2cdfaf593ec0cb2c92b8f7d22b5ceaeec9405ac1
mohamed ali <mohammedali.saisdubai@gmail.com>
2013-03-07 14:43:22 -0800

    Added comment ilne1

--- main.c
+++ main.c
@@ -1,31 +1,19 @@
+//
+//  main.c
+//  eece315_assignment4
+//
+//  Created by Mohamed Ali on 2012-11-22.
+//  Copyright (c) 2012 Mohamed Ali. All rights reserved.
+//
+//line1
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-
-//By:Mohamed Ali
-//s.54809108
-//GIT TRIAL
-int main(int argc, char * argv[])
+int main(int argc, const char * argv[])
 {
-    if(argc!= 2) //check input arguments
-    {
-        printf("ERROR: invalid input\n");
-        return -1;
-    }
-   
-   
-    unsigned long long  int input;
-    unsigned long long  int page;
-    unsigned long long  int offset;
-    
-   
-    
-    input = atol ( argv[1] ); //convert the input from char to int
-    page = input / 4096;
-    offset = input % 4096;
 
-    printf("The address %lld contains:\npage number = %lld\noffset = %lld\n",input,page, offset);
-   
+    // insert code here...
+    printf("Hello, World!\n");
     return 0;
 }
+
